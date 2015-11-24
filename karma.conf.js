d9e5a7dc15c894d99d3a49c2ac1a8e0722282602
byavv <aksslavvv@gmail.com>
2015-11-24 11:51:24 +0300

    karma configured

---
+++ karma.conf.js
@@ -0,0 +1,44 @@
+var webpackConfig = require("./server/config/webpack")({mode:"test", rootpath:"/"});
+var webpack = require("webpack");
+
+module.exports = function(config) {
+    config.set({
+        basePath: "",
+        frameworks: ["jasmine"],
+        files: [
+            // needed because of this issue: https://github.com/webpack/style-loader/issues/31
+            './node_modules/phantomjs-polyfill/bind-polyfill.js',
+            { pattern: "spec.bundle.js", watched: false }
+        ],
+        webpack: webpackConfig,
+        webpackMiddleware: {
+            noInfo: false,           
+            stats: {
+                colors: true
+            }
+        },
+        coverageReporter: {
+            type: 'html',
+            dir: 'tests/coverage/'
+        },
+        exclude: [],
+        captureTimeout: 60000,
+        preprocessors: {
+            'spec.bundle.js': ["webpack"],
+        },
+        singleRun: true,
+        reporters: ["mocha", "coverage"],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        browsers: ["PhantomJS"],
+        plugins: [ 
+            require("karma-webpack"),
+            "karma-jasmine",
+            "karma-chrome-launcher",
+            "karma-phantomjs-launcher",
+            "karma-mocha-reporter",
+            "karma-js-coverage"
+        ]
+    });
+};
