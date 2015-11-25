fb9457324f659154849ae61c9e6f56de6240ca8c
byavv <aksslavvv@gmail.com>
2015-11-25 13:08:32 +0300

    user api controller + tests

--- server/controllers/user.api.controller.js
+++ server/controllers/user.api.controller.js
@@ -1,15 +1,214 @@
-var mongoose = require("mongoose");
+var mongoose = require("mongoose"),
+    nodemailer = require("nodemailer"),
+    _ = require("lodash"),
+    crypto = require("crypto"),
+    User = mongoose.model("User"),
+    async = require("async"),
+    mailHelper = require("../helpers/mail.helper"),
+    nconf = require("nconf");
+    
 
 //di for testing
-module.exports = function (user, jwt) {
-    var User;
-    if(!user){
-        User = mongoose.model("User");
-    }
-    if(!jwt){
-        
-    }
-    return {
-       
+module.exports = {
+    /** 
+     * Route callback for GET /api/reset/:token
+     * When user clicks on link in email to reset password.
+     */
+    validateResetToken: function (req, res, next) {
+        User.findOne({
+            "resetData.resetToken": req.params.token,
+            "resetData.resetExpires": {
+                $gt: Date.now()
+            }
+        }, (err, user) => {
+            if (err) res.status(500).send();
+            if (user) {
+                res.redirect('/password/reset/' + req.params.token)
+            } else {
+                res.redirect('/password/error/');
+            }
+        });
+    },
+    postAccount: function (req, res) {
+        User.findOne({
+            _id: req.user.id
+        }, (err, user) => {
+            if (err) return res.status(500).send();
+            if (!user) {
+                return res.status(400).send();
+            } else {
+                var account = {
+                    username: user.username,
+                    email: user.email,
+                    provider: user.provider
+                };
+                return res.status(200).json(account);
+            }
+        });
+    },
+    /**
+	* Change account data
+	*/
+    updateAccount: (req, res, next) => {
+        var userId = req.user.id;
+        if (userId && req.body.account) {
+            User.findOne({ _id: userId }, function (err, user) {
+                if (err) return next(err);
+                if (!user) {
+                    res.status(400).send({ key: "error_user_found" });
+                }
+                var oldPsw = req.body.account.oldpassword;
+                if (user.authenticate(oldPsw)) {
+                    if (!!req.body.account.password) {
+                        user.password = req.body.account.password;
+                    } else {
+                        user.password = oldPsw;
+                    }
+                    user.email = req.body.account.email;
+                    user.username = req.body.account.username;
+                    user.save((err) => {
+                        if (err) {
+                            var errList = [];
+                            if (err.name === "ValidationError" && err.errors) {
+                                errList = _.values(err.errors).map((error) => {
+                                    return error.message;
+                                });
+                                return res.status(400).send({
+                                    key: "error_validation",
+                                    message: errList
+                                });
+                            } else {
+                                return res.status(500).send({ key: "error_500" });
+                            }
+
+                        } else {
+                            res.status(200).send({ key: "info_password_changed_success" });
+                        }
+                    })
+                } else {
+                    res.status(400).send({ key: "error_old_password_is_not_valid" });
+                }
+            })
+        }
+    },
+    /**
+     * Get user profile data
+     * middleware for POST route: /profile/:id
+     */
+    postProfile: function (req, res) {
+        User.findOne({
+            _id: req.user.id
+        }, (err, user) => {
+            if (err) return res.status(500).send()
+            if (!user) {
+                return res.status(400).send();
+            } else {
+                return res.status(200).json(user.profile);
+            }
+        });
+    },
+    /**
+	 * Change user profile data
+	 * */
+    updateProfile: function (req, res, next) {
+        var userId = req.user.id;
+        if (userId && req.body.profile) {
+            User.findOne({ _id: userId }, function (err, user) {
+                if (err) return next(err);
+                if (!user) {
+                    res.status(400).send({ key: 'error_user_found' });
+                }
+                _.extend(user.profile, req.body.profile);
+                user.save(function (err) {
+                    if (err) return next(err);
+                    res.status(200).json({ key: "info_profile_updated_success", profile: user.profile });
+                })
+            })
+        }
+    },
+    /**
+     * Send email with instructions to reset password     * 
+     */
+    forgot: function (req, res, next) {
+        async.waterfall([
+            // generate random reset token
+            (done) => {                
+                crypto.randomBytes(20, function (err, buffer) {
+                    var token = buffer.toString('hex');
+                    done(err, token);
+                });
+            },
+            // save reset token in user profile
+            (token, done) => {                
+                if (req.body.email) {
+                    User.findOne({
+                        email: req.body.email
+                    }, (err, user) => {
+                        if (!user) {
+                            return res.status(400).send({ key: 'error_user_found' });
+                        }
+                        user.resetData.resetToken = token;
+                        user.resetData.resetExpires = Date.now() + 3600000;
+                        user.save((err) => {
+                            done(err, token, user);
+                        });
+                    })
+                } else {
+                    return res.status(400).send({ key: "error_email_field_empty" });
+                }
+            },
+            // render email
+           (token, user, done) => {               
+                res.render('templates/reset-password-email', {
+                    name: user.username,
+                    appName: nconf.get("appName"),
+                    url: 'http://' + req.headers.host + '/api/reset/' + token
+                }, (err, emailHTML) => {
+                    done(err, emailHTML, user);
+                });
+            },
+            // send it
+            (emailHTML, user, done) => {
+                mailHelper.sendResetMail(
+                    user.email,                 // to
+                    nconf.get("mailer").from,   // from
+                    emailHTML,                  // what
+                    nconf.get("mailer").options,// options
+                    (err) => {
+                        if (!err)
+                            return res.status(200).send({ key: "info_password_change_mail_sent" });
+                        done(err);
+                    })
+            }
+        ], (err) => {
+            console.log(err);
+            if (err) return next(err);
+        });
+    },
+    /**
+	 * Change user password using reset password form
+	 */
+    resetForgotPassword: (req, res) => {
+        var newPassword = req.body.password;
+        var token = req.body.token;
+        if (newPassword && token) {
+            User.findOne({ "resetData.resetToken": token }, (err, user) => {
+                if (!err && user) {
+                    user.password = newPassword;
+                    user.resetData = undefined;
+                    user.save(function (err) {
+                        if (err) {
+                            return res.status(500).send({ key: "error_500" });
+                        } else {
+                            res.status(200).send({ key: "info_password_changed_success" });
+                        }
+                    });
+                } else {
+                    res.status(400).send({ key: "error_user_found" });
+                }
+            });
+        } else {
+            res.status(400).send({ key: "error_notValidCredentials" });
+        }
     }
 }
---
+++ test/server/controllers/user.api.controller.spec.js
@@ -0,0 +1,158 @@
+var request = require('supertest'),
+	express = require('express'),
+	sinon = require("sinon"),
+	chai = require("chai"),
+	rewire = require("rewire"),
+	expressConf = require("../../../server/config/express"),
+	assert = chai.assert,
+	expect = chai.expect;
+
+chai.should();
+describe("User api controller tests", () => {
+	var app;
+	var mockUser, mockMailHelper;
+	var findOneStub, saveStub, sendResetMailstub;
+	var controller;
+	var user_to_find;
+	before((done) => {				
+		//user to be returnen as finded in db mock
+		user_to_find = {
+			_id: "123456789",
+			username: "John",
+			profile: "github",
+			authenticate: sinon.stub().returns(true),
+			resetData: {},
+			save: sinon.stub().yields(null, { _id: 12587458 }),
+		}
+		findOneStub = sinon.stub().yields(null, user_to_find);
+		saveStub = sinon.stub().yields(null, { _id: "0254879456" });
+				
+		//mongoose User model mock
+		mockUser = function () {
+			this.save = saveStub;
+
+		};
+		mockUser.findOne = findOneStub;
+		sendResetMailstub = sinon.stub().yields(null);
+		mockMailHelper = {
+			sendResetMail: sendResetMailstub
+		}
+		
+		controller = rewire("../../../server/controllers/user.api.controller");
+		controller.__set__("User", mockUser);
+		controller.__set__("mailHelper", mockMailHelper);
+
+		done();
+	})
+	before(() => {
+		app = express();
+		expressConf(app);
+		//mock jwt payload extraction
+		app.use("/api", (req, res, next) => {
+			req.user = { id: "12589541025", roles: ["user"] }
+			next();
+		});
+		//fake routes
+		app.get('/api/reset/:token', controller.validateResetToken);
+		app.post('/api/account', controller.postAccount);
+		app.put('/api/profile', controller.postProfile);
+		app.post('/api/forgot', controller.forgot);
+
+	});
+
+  
+	describe("Reset password test", () => {
+		beforeEach(()=>{
+			//findOneStub = sinon.stub().yields(null, user_to_find);
+		})		
+		afterEach(()=>{
+			findOneStub.reset();
+			sendResetMailstub.reset();
+		})
+		it("Should redirect browser to angular route with given token as param", (done) => {
+			findOneStub.yields(null, user_to_find);
+			request(app)
+				.get('/api/reset/fake_token_from_email')
+				.expect(302)
+				.end((err, res) => {
+					expect(res.status).to.be.equal(302);
+					expect(findOneStub.called).to.be.equal(true);
+					expect(res.header['location']).to.be.equal("/password/reset/fake_token_from_email");
+					done();
+				});
+
+		})
+		it("Should redirect to error page if mongo error", (done) => {
+			findOneStub.yields(new Error("Something awful has happened"), null);
+			request(app)
+				.get('/api/reset/fake_token_from_email')
+				.expect(500)
+				.end((err, res) => {
+					expect(res.status).to.be.equal(500);
+					expect(findOneStub.called).to.be.equal(true);
+					done();
+				});
+		})
+		it("Should redirect to error page if user with given token is not found", (done) => {
+			//user with given reset token is not found, or expired
+			findOneStub.yields(null, null);
+			request(app)
+				.get('/api/reset/fake_token_from_email')
+				.expect(302)
+				.end((err, res) => {
+					expect(res.status).to.be.equal(302);
+					expect(findOneStub.called).to.be.equal(true);
+					expect(res.header['location']).to.be.equal("/password/error/");
+					done();
+				});
+		})
+		it("Should send email with reset instructions", (done) => {			
+			sendResetMailstub.yields(null);
+			findOneStub.yields(null, user_to_find);
+			request(app)
+				.post('/api/forgot')
+				.send({ email: "fake@email.com" })
+				.expect(200)
+				.end((err, res) => {
+					expect(res.status).to.be.equal(200);
+					expect(findOneStub.calledOnce).to.be.equal(true);					
+					expect(sendResetMailstub.calledOnce).to.be.equal(true);						
+					done();
+				});
+		})
+	})
+	
+	
+	describe("Account and profile callbacks tests", () => {
+		beforeEach(()=>{
+			//findOneStub = sinon.stub().yields(null, user_to_find);
+		})
+		afterEach(()=>{
+			findOneStub.reset();
+		})
+		it("Should return account data", (done) => {
+			findOneStub.yields(null, user_to_find);			
+			request(app)
+				.post('/api/account')
+				.expect(200)
+				.end((err, res) => {
+					expect(res.status).to.be.equal(200);
+					expect(findOneStub.calledOnce).to.be.equal(true);
+					expect(res.body).to.have.property("username");
+					done();
+				});
+		})
+		it("Should return profile data", (done) => {	
+			findOneStub.yields(null, user_to_find);		
+			request(app)
+				.put('/api/profile')
+				.expect(200)
+				.end((err, res) => {
+					expect(res.status).to.be.equal(200);
+					expect(findOneStub.calledOnce).to.be.equal(true);
+					done();
+				});
+		})
+	})
+
+})
