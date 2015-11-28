89634ae36932c8fc65a81476b1f00614cd4f4f40
byavv <aksslavvv@gmail.com>
2015-11-28 13:12:07 +0300

    loader directive refactor

--- client/app/directives/loader/loader.directive.ts
+++ client/app/directives/loader/loader.directive.ts
@@ -2,24 +2,24 @@ require("./loader.less");
 export default class LoaderDirective implements ng.IDirective {
     public restrict = "E";
     public template =
-        '<div class="loader-container">' +
-            '<div class="loader">' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-                '<div class= "loader-block"> </div>' +
-            ' </div>' +
-        ' </div>';
+    `<div class="loader-container">
+            <div class="loader">
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+                <div class= "loader-block"> </div>
+             </div>
+         </div>`;
 }
